be5625d6b2d5b810ac771850f5517f87febf7682
xross <xross@users.noreply.github.com>
2024-09-27 00:11:01 +0000

    ✅ Ready to clone and code.

--- test/cli.py
+++ test/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for test project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m test` and `$ test `.
 
     This is your program's entry point.
 
